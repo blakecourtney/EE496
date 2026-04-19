516a259a7c16242a8bc4c27753798d2c70bc6236
Ashly Lam <ashlylam@usc.edu>
2026-04-19 13:53:43 -0700

    update serial testing

--- ashly_code/main.cpp
+++ ashly_code/main.cpp
@@ -26,48 +26,6 @@ int main(){
     cout << " flag " << result.update_backbone << endl;
 
     
-// 1. Open serial port (CHANGE THIS to your port)
-int fd = open("/dev/cu.usbserial-0001");
-
-if (fd < 0) {
-    cout << "Failed to open serial port\n";
-    return 1;
-}
-else {
-    cout << "Succeeded to open serial port\n";
-}
-
-    // // 2. Configure serial (115200 baud like your Python)
-    // struct termios tty;
-    // memset(&tty, 0, sizeof tty);
-
-    // tcgetattr(fd, &tty);
-    // cfsetospeed(&tty, B115200);
-    // cfsetispeed(&tty, B115200);
-
-    // tty.c_cflag |= (CLOCAL | CREAD);
-    // tty.c_cflag &= ~CSIZE;
-    // tty.c_cflag |= CS8;     // 8-bit
-    // tty.c_cflag &= ~PARENB; // no parity
-    // tty.c_cflag &= ~CSTOPB; // 1 stop bit
-
-    // tcsetattr(fd, TCSANOW, &tty);
-
-    // // 3. Build packet
-    // auto packet = build_waypoint_packet(
-    //     1,              // drone_id
-    //     34.0522f,      // lat
-    //     -118.2437f,    // lon
-    //     50.0f          // alt
-    // );
-
-    // // 4. Send it
-    // write(fd, packet.data(), packet.size());
-
-    // std::cout << "Sent waypoint packet\n";
-
-    // // 5. Close
-    // close(fd);
 
 
     return 0;
